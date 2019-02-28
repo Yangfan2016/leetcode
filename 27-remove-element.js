709acd1072aa2a2160562c9b07cd80aa5589e252
Yangfan <15234408101@163.com>
2019-02-28 11:56:47 +0800

    fix: 27

--- 27-remove-element.js
+++ 27-remove-element.js
@@ -3,14 +3,14 @@
  * @param {number} val
  * @return {number}
  */
-var removeElement = function(nums, val) {
-    if (nums.length===0) return 0;
+var removeElement = function (nums, val) {
+    if (nums.length === 0) return 0;
+    let c = 0;
     for (let i = 0; i < nums.length; i++) {
-        let item = nums[i];
-        if (item === val) {
-            nums.splice(i, 1);
-            i--;
+        if (nums[i] !== val) {
+            nums[c] = nums[i];
+            c++;
         }
     }
-    return nums.length;
+    return c;
 };
