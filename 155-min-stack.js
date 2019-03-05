333a3830ca07387e36aef8a2d9ae3292a73f37c1
yangfan2016 <15234408101@163.com>
2019-03-05 11:29:33 +0800

    update: 155-fast

--- 155-min-stack.js
+++ 155-min-stack.js
@@ -1,50 +1,47 @@
 /**
  * initialize your data structure here.
  */
-var MinStack = function() {
-    this.minIndex=0;
-    this.list=[];
+var MinStack = function () {
+    this.stack = [];
+    this.min = Infinity;
 };
 
 /** 
  * @param {number} x
  * @return {void}
  */
-MinStack.prototype.push = function(x) {
-    this.list.push(x);
+MinStack.prototype.push = function (x) {
+    if (x <= this.min) {
+        this.stack.push(this.min); // record last minium value
+        this.min = x; // set new minimum value
+    }
+    this.stack.push(x); // push stack
 };
 
 /**
  * @return {void}
  */
-MinStack.prototype.pop = function() {
-    this.list.pop();
+MinStack.prototype.pop = function () {
+    if (this.stack.pop() === this.min) { // remove last value
+        this.min = this.stack.pop(); // reset second last for minimum value
+    }
 };
 
 /**
  * @return {number}
  */
-MinStack.prototype.top = function() {
-    let len=this.list.length;
-    return this.list[len-1];
+MinStack.prototype.top = function () {
+    return this.stack[this.stack.length - 1];
 };
 
 /**
  * @return {number}
  */
-MinStack.prototype.getMin = function() {
-    this.minIndex=0; // reset
-    
-    this.list.forEach((item,n)=>{
-        let min=this.list[this.minIndex];
-        if (item<min) {
-            this.minIndex=n;
-        }
-    });
-    return this.list[this.minIndex];
+MinStack.prototype.getMin = function () {
+    return this.min;
 };
 
-/** 
+/**
  * Your MinStack object will be instantiated and called as such:
  * var obj = Object.create(MinStack).createNew()
  * obj.push(x)
