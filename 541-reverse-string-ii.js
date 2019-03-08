45726828d75b53606c5256fc37a018cd6fc0d75a
yangfan2016 <15234408101@163.com>
2019-03-08 14:01:25 +0800

    update: 541-fast

--- 541-reverse-string-ii.js
+++ 541-reverse-string-ii.js
@@ -14,29 +14,22 @@ var reverseStr = function (s, k) {
 
     let last = '';
     let left = '';
-    let n = 1;
 
-    for (let i = 0, len = s.length; i < len; i++) {
-        if (i === n * 2 * k - 1) {
-            last += s[i];
-            // 没 2*n*k 收集一组
-            let r = reverseAllStr(last.substr(0, k)) + last.substr(k);
-            left += r;
-            // 进行翻转操作
-            last = '';
-            // 清空收集器
-            n++;
-            // 加倍 2k->4k，进行下一组
-        } else {
-            last += s[i];
-            // 收集剩余字符串
-        }
+    for (var i = 0; i + 2 * k - 1 < len; i = i + 2 * k) { // 4  8  12 
+        let str = s.substr(i, 2 * k);
+        let l = str.substr(0, k); // 0, 2   4,6
+        let r = str.substr(k); // 2      6
+        l = reverseAllStr(l);
+        left += l + r;
     }
+
+    last = s.substr(i);
 
     return left + reverseStr(last, k);
 };
 
 function reverseAllStr(s) {
+
     let res = '';
     for (let i = 0, len = s.length; i < len; i++) {
         res = s[i] + res;
