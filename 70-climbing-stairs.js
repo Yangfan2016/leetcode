91cf02fad35d541c883cb2783a8d8cddd967d7c3
yangfan2016 <15234408101@163.com>
2019-03-27 21:12:45 +0800

    update: 70-method2

--- 70-climbing-stairs.js
+++ 70-climbing-stairs.js
@@ -2,31 +2,17 @@
  * @param {number} n
  * @return {number}
  */
-var climbStairs = function (n) {
-    //【数学】排列组合
-    // 2x+y=n
 
-    let middle = n >> 1;
-    let x = 0;
-    let y = 0;
-    let c = 0;
+let cache=[1,2];
 
-    while (x <= middle) {
-        y = n - 2 * x;
-        c += combine(x, y + x);
-        x++;
-    }
-
-    return c;
+var climbStairs = function(n) {
+   if (n<1) return 0; // 极端情况
+   if (n<3) return cache[n-1]; // 极端情况
+   if (cache[n-1]) return  cache[n-1]; // 从缓存里取值
+    
+   cache[n-1]=climbStairs(n-1)+climbStairs(n-2); // 存入缓存
+    
+   return cache[n-1];
 };
-// 组合
-function combine(m, n) {
-    return factorial(n) / (factorial(m) * factorial(n - m));
-}
-// 阶乘
-function factorial(n) {
-    if (n < 2) return 1;
 
-    return n * factorial(n - 1);
-}
 
