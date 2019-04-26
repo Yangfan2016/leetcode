70d21a2765f74179d36a1a81131473e77a893fb8
yangfan2016 <15234408101@163.com>
2019-04-26 09:45:22 +0800

    fix(git-setting): crlf->lf

--- 1-two-sum.js
+++ 1-two-sum.js
@@ -6,7 +6,7 @@
 var twoSum = function (nums, target) {
     let len = nums.length;
     let map = {}; // 结构 {b:a 的索引}   target===a+b
- 
+
     for (let i = 0; i < len; i++) {
         let rest = target - nums[i]; // 7=9-2
         if (map[nums[i]] > -1) {   // map[7]=0
