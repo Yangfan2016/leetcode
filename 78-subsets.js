b58d28d97909a1a7371255b9ab0d4605f65714ff
yangfan2016 <15234408101@163.com>
2019-05-07 10:49:37 +0800

    update: 78-fast

--- 78-subsets.js
+++ 78-subsets.js
@@ -1,4 +1,17 @@
 /**
+ * 
+ *  幂集  
+ *  利用二进制，列出所有情况
+ *      abc	Subset
+ *  0	000	{}
+ *  1	001	{c}
+ *  2	010	{b}
+ *  3	011	{c, b}
+ *  4	100	{a}
+ *  5	101	{a, c}
+ *  6	110	{a, b}
+ *  7	111	{a, b, c} 
+ * 
  * @param {number[]} nums
  * @return {number[][]}
  */
@@ -10,43 +23,20 @@ var subsets = function (nums) {
     if (nums.length === 2)
         return [[], nums, [nums[0]], [nums[1]]];
 
-
-    // 幂集  
-    // 利用二进制，列出所有情况
-    //      abc	Subset
-    // 0	000	{}
-    // 1	001	{c}
-    // 2	010	{b}
-    // 3	011	{c, b}
-    // 4	100	{a}
-    // 5	101	{a, c}
-    // 6	110	{a, b}
-    // 7	111	{a, b, c}    
-
-    // 转换为二进制
-    function d2bin(n) {
-        return n.toString(2);
-    }
-    // 根据二进制的位置填充子集
-    function fill(arr, map) {
-        let res = [];
-
-        for (let i = 0, len = arr.length; i < len; i++) {
-            if (map[i] === "1") {
-                res.push(arr[i]);
-            }
-        }
-
-        return res;
-    }
-
     let len = nums.length;
-    let total = 2 ** len; // 所有情况的总数
+    let total = 2 ** len; // 所有的子集的情况
     let result = [];
 
     for (let i = 0; i < total; i++) {
-        let bin = d2bin(i).padStart(len, "0");
-        result.push(fill(nums, bin));
+        let group = [];
+        for (let j = 0; j < len; j++) {
+            // 计算出 i 的二进制 的第 j 位是否为 1（即有值）
+            // 即 (i & (1 << j)) > 0
+            if (i & 1 << j) {
+                group.push(nums[j]); // 存入每一组
+            }
+        }
+        result.push(group);
     }
 
     return result;
