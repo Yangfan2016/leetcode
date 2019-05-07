62369c9b3824528a89dfd17eb6c5942a9d79f88a
yangfan2016 <15234408101@163.com>
2019-05-07 17:06:47 +0800

    add: 717

---
+++ 717-1-bit-and-2-bit-characters.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} bits
+ * @return {boolean}
+ */
+var isOneBitCharacter = function (bits) {
+    if (bits.length === 1) return true;
+
+    let len = bits.length;
+
+    for (let i = 0; i < len; i++) {
+        let item = bits[i];
+        if (item === 1) { // 如果是 1 ，则将本元素和前一位元素 “配对”
+            bits[i] = -1;
+            bits[i + 1] = -1;
+        }
+    }
+
+    return bits[len - 1] !== -1; // 判断最后一位是否被 “配对”，来确定是否是 一比特字符
+};
