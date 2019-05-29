89b0b367abd6de0bb359f505ff35c78572d7409f
Yangfan <15234408101@163.com>
2019-05-29 16:42:35 +0800

    update: comment

--- 345-reverse-vowels-of-a-string.js
+++ 345-reverse-vowels-of-a-string.js
@@ -19,14 +19,14 @@ var reverseVowels = function (s) {
   while (h < r) {
     // 头指针小于尾指针
     if (!pattern.test(arr[h])) {
-      // 如果是非字母，头指针向后移动一位
+      // 如果是非元音字母，头指针向后移动一位
       h++;
       continue;
       // 跳过本次循环
     }
 
     if (!pattern.test(arr[r])) {
-      // 如果是非字母，尾指针向前移动一位
+      // 如果是非元音字母，尾指针向前移动一位
       r--;
       continue;
       // 跳过本次循环
@@ -35,7 +35,7 @@ var reverseVowels = function (s) {
     if (h < r) {
       // 头指针小于尾指针
       [arr[h], arr[r]] = [arr[r], arr[h]];
-      // 如果都是字母，进行位置互换
+      // 如果都是元音字母，进行位置互换
     }
     // 前进一位，进行下次操作
     h++;
